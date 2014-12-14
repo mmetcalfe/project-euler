a0217006f86f862c0c903cf20ae6c84fae56640e
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2014-12-14 14:49:44 +1100

    Move problems between folders

---
+++ unsolved/problem006_sum_square_differen.hs
@@ -0,0 +1,21 @@
+--
+-- Problem 6: Sum square difference
+-- (Published on Friday, 14th December 2001, 06:00 pm; Solved by 194702)
+--
+--     The sum of the squares of the first ten natural numbers is,
+-- 1^{2} + 2^{2} + ... + 10^{2} = 385
+--     The square of the sum of the first ten natural numbers is,
+-- (1 + 2 + ... + 10)^{2} = 55^{2} = 3025
+--     Hence the difference between the sum of the squares of the first
+-- 	ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+--
+--     Find the difference between the sum of the squares of the first
+-- 	one hundred natural numbers and the square of the sum.
+
+-- n = 100
+-- nums = range(n + 1)
+-- sum_squares = sum([k ** 2 for k in nums])
+-- squared_sum = sum([k for k in nums]) ** 2
+-- print('Sum of squares:', sum_squares)
+-- print('Square of sum:', squared_sum)
+-- print('Difference:', squared_sum - sum_squares)
---
+++ unsolved/problem010_summation_of_prim.hs
@@ -0,0 +1,31 @@
+--
+-- Problem 10: Summation of primes
+-- (Published on Friday, 8th February 2002, 06:00 pm; Solved by 132998)
+--
+--     The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+--
+--     Find the sum of all the primes below two million.
+--
+-- <p class="info">Note: This problem has been changed recently, please
+-- 	check that you are using the right parameters.</p>
+
+-- import math
+-- n = 2000000
+-- def sieve_of_eratosthenes():
+--     nums = []
+--     primes = []
+--     for k in range(1, (n//2) + 1):
+--         v = 2*k + 1
+--         nums.append(v)
+--     primes.append(2)
+--     while True:
+--         v = nums[0]
+--         if v > int(math.sqrt(n) + 1):
+--             for k in nums:
+--                 primes.append(k)
+--             return primes
+--         primes.append(v)
+--         nums = [ k for k in nums if k % v != 0 ]
+-- s = sieve_of_eratosthenes()
+-- print(s)
+-- print(sum(s))
