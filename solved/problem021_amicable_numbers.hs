f5d4f30857d0223abd1196401362f33935a7bb51
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-01-05 10:48:23 +1100

    PE-021: Solve.

--- solved/problem021_amicable_numbers.hs
+++ solved/problem021_amicable_numbers.hs
@@ -1,17 +1,17 @@
--- 
+--
 -- Problem 21: Amicable numbers
 -- (Published on Friday, 5th July 2002, 06:00 pm; Solved by 77726)
--- 
+--
 --     Let d(n) be defined as the sum of proper divisors of n (numbers
--- 	less than n which divide evenly into n).
--- 
+--   less than n which divide evenly into n).
+--
 -- If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable
--- 	pair and each of a and b are called amicable numbers.
--- 
+--   pair and each of a and b are called amicable numbers.
+--
 --     For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11,
--- 	20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors
--- 	of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
--- 
+--   20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors
+--   of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+--
 --     Evaluate the sum of all the amicable numbers under 10000.
 
 import Primes
@@ -27,11 +27,15 @@ isAbundant :: Integer -> Integer -> Bool
 isAbundant n = (>n) . sum . divisors
 
 isLowerAmicable n =
-    let s = (sum . divisors) n
-        if s > n
-            then False
-            else
+    let s = amicableSibling n
+    in if s <= n
+         then False
+         else n == amicableSibling s
 
 main = do
-    let n = 100
-    print $ divisors 100
+    let n = 10000
+    let la = filter isLowerAmicable [1..n]
+    let pairs = map (\x -> (x, amicableSibling x)) la
+    print $ la
+    print $ pairs
+    print $ sum $ map (\(a,b) -> a+b) pairs
