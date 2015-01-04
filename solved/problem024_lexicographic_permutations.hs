b06c05dcba56d5000a2fcdd4ecd44f5a930f8251
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-01-05 10:26:46 +1100

    PE-024: Solve.

--- solved/problem024_lexicographic_permutations.hs
+++ solved/problem024_lexicographic_permutations.hs
@@ -1,15 +1,15 @@
--- 
+--
 -- Problem 24: Lexicographic permutations
 -- (Published on Friday, 16th August 2002, 06:00 pm; Solved by 62517)
--- 
+--
 --     A permutation is an ordered arrangement of objects. For example,
 -- 	3124 is one possible permutation of the digits 1, 2, 3 and 4. If
 -- 	all of the permutations are listed numerically or alphabetically,
 -- 	we call it lexicographic order. The lexicographic permutations of
 -- 	0, 1 and 2 are:
--- 
+--
 --     012   021   102   120   201   210
--- 
+--
 --     What is the millionth lexicographic permutation of the digits 0,
 -- 	1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
@@ -24,7 +24,7 @@ factorialAbove :: Integer -> Integer
 factorialAbove n = fromJust $ find ((>n) . factorial) [1 ..]
 
 decomposeFactoradic :: Integer -> Integer -> [Integer]
-decomposeFactoradic 0 _ = []
+decomposeFactoradic 0 _ = [0]
 decomposeFactoradic k n =
     let (d, n') = divMod n (factorial k)
     in d : decomposeFactoradic (k - 1) n'
@@ -42,20 +42,21 @@ toPermutation :: [Integer] -> [a] -> [a]
 toPermutation [] _ = []
 toPermutation _ [] = []
 toPermutation f s =
-    let i:fs = f
+    let i:fs = padLeft (length s) 0 f
         (e, s') = removeAt i s
     in e : toPermutation fs s'
 
 padLeft n e l = replicate (n - length l) e ++ l
 
 main = do
-    let n = 4
-    let k = factorialAbove n
-    let f = factoradic n
-    print $ factorialAbove n
-    print $ factorial k
-    print $ f
-    print $ toPermutation (padLeft 3 0 f) "012a"
---     print $ toPermutation (padLeft 10 0 f) "0123456789a"
---     print $ map factoradic [1..6]
---     print $ map (flip toPermutation "012" . factoradic) [1..6]
+    let n = 1000000
+    let s = "0123456789"
+    let f = factoradic (n - 1)
+    print $ toPermutation f s
+    -- print $ factorialAbove n
+    -- print $ factorial k
+    -- print $ factoradic 719
+    -- print $ factoradic 720
+    -- print $ f
+    -- print $ map (padLeft (length s) 0 . factoradic) [0..5]
+    print $ map (flip toPermutation s . factoradic) [0..5]
