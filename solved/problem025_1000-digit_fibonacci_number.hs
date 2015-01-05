3663f1375d8d738aa1a59fd49e45ec700b57c8e4
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-01-05 21:11:24 +1100

    PE-025: Solve.

--- solved/problem025_1000-digit_fibonacci_number.hs
+++ solved/problem025_1000-digit_fibonacci_number.hs
@@ -32,6 +32,8 @@ import Data.Maybe
 
 phi :: Double
 phi = (1.0 + sqrt 5) / 2.0
+psi :: Double
+psi = 1.0 - phi
 
 approxNthFib :: Integer -> Double
 approxNthFib n = (phi ^ n) / sqrt 5
@@ -39,9 +41,22 @@ approxNthFib n = (phi ^ n) / sqrt 5
 numDigits :: Double -> Integer
 numDigits n = 1 + floor (logBase 10 n)
 
+approxNumDigitsNthFib :: Integer -> Integer
+approxNumDigitsNthFib n = 1 + floor (fromIntegral n * logBase 10.0 phi - logBase 10 (sqrt 5))
+
+log10 = logBase 10
+numDigitsNthFib :: Double -> Double
+numDigitsNthFib n = 1 + n*log10 phi + log10 (1 - (psi/phi)**n) - log10 (sqrt 5)
+
 main = do
-    let n = fromJust $ find ((>=1000) . numDigits . approxNthFib) [1..]
-    print n
-    print $ numDigits $ fromIntegral n
-    print $ approxNthFib $ fromIntegral n
-    print $ map (approxNthFib) [1..1475]
+    let nc = fromJust $ find ((>=1000) . numDigits . approxNthFib) [1..]
+    let na = fromJust $ find ((>=1000) . approxNumDigitsNthFib) [1..]
+    let ne = fromJust $ find ((>=1000) . numDigitsNthFib) [1..]
+    print nc
+    print na
+    print ne
+    print $ approxNumDigitsNthFib $ fromIntegral nc
+    print $ approxNumDigitsNthFib $ na
+    let r = [ne-10..ne+10]
+    print $ zip r $ map (approxNumDigitsNthFib . round) r
+    print $ zip r $ map (numDigitsNthFib) r
