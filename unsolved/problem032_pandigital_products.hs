1e139591d29eb3700957fca1849e03bdb94f0e41
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-01-18 20:58:55 +1100

    PE-032: Potential solution.

--- unsolved/problem032_pandigital_products.hs
+++ unsolved/problem032_pandigital_products.hs
@@ -17,11 +17,30 @@
 -- 	to only include it once in your sum.
 
 -- a * b = c
--- num digits must add to 10
+-- num digits must add to 9
 -- cannot repeat digits
 -- require a < b (symmetry)
 -- upper limit (digits) to product?
---   - < 9 (must have a product)
+--   - < 8 (must have a product)
 --   - < 5 (99*99 = 9801)
 --   - >= 4 (99*99 = 9801)
 --   - == 4
+--   - 1234 <= c <= 9876
+
+import Data.List (sort, union)
+
+numDigits :: Integer -> Integer
+numDigits n =  1 + round (logBase 10 (fromIntegral n))
+
+isPandigitalString :: String -> Bool
+isPandigitalString s = sort s == "123456789"
+
+isPandigitalProduct a b = isPandigitalString (show a ++ show b ++ show (a*b))
+
+searchRange n = [1234 `div` n..9876 `div` n]
+
+main = do
+    let pairs = filter (uncurry isPandigitalProduct) [(a, b) | a <- [1..100], b <- searchRange a]
+        products = map (uncurry (*)) pairs
+    print $ zip pairs products
+    print $ sum (union [] products)
