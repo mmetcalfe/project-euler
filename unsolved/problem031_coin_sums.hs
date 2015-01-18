1ac4dfd5152faf3380558ac4971ecb589f2439e1
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-01-19 00:02:08 +1100

    PE-031: Brute force code.

--- unsolved/problem031_coin_sums.hs
+++ unsolved/problem031_coin_sums.hs
@@ -11,5 +11,37 @@
 -- 	coins?
 
 -- num solutions to integer linear program:
--- 1a + 2b + 5c + 10d + 20e + 50f + 100g + 200g = 200
--- a,b,c,d,e,f,g >= 0
+-- 1a + 2b + 5c + 10d + 20e + 50f + 100g + 200h = 200
+-- a <= 200
+-- b <= 100
+-- c <= 40
+-- d <= 20
+-- e <= 10
+-- f <= 50
+-- g <= 2
+-- h <= 1
+-- a,b,c,d,e,f,g,h >= 0
+
+type Change = [(Integer, Integer)]
+
+denominations = [2, 5, 10, 20, 50, 100, 200]
+
+emptyChange :: Change
+emptyChange = map ((,) 0) denominations
+
+value :: Change -> Integer
+value c = sum $ map (uncurry (*)) c
+
+nextChange :: Integer -> Change -> Change
+nextChange k [] = []
+nextChange k ((v,d):r)
+    | v < (k `div` d) = (v + 1, d) : r
+    | otherwise = (0, d) : nextChange k r
+
+changeSets k = iterate (nextChange k) emptyChange
+
+main = do
+    let k = 200
+        numSets = product (map (fromIntegral . div k) denominations)
+    print numSets
+    print $ take numSets (changeSets k)
