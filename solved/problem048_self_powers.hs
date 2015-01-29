2013b788e0f372824f8301a0ae6e813edac21ba2
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-01-29 23:00:27 +1100

    PE-048: Solve.

--- solved/problem048_self_powers.hs
+++ solved/problem048_self_powers.hs
@@ -6,3 +6,9 @@
 -- 
 --     Find the last ten digits of the series, 1^{1} + 2^{2} + 3^{3} +
 -- 	... + 1000^{1000}.
+
+powMod :: Integer -> Integer -> Integer
+powMod n k = foldr (\x y -> x*y `mod` 10^10) 1 (replicate (fromIntegral k) n)
+
+main = do
+    print $ (sum $ map (\x -> powMod x x) [1..1000]) `mod` 10^10
