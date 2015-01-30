19d4226cab6a8484cd59795184c98d59fc7a0a86
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-01-30 13:27:38 +1100

    PE-047: Solve.

--- solved/problem047_distinct_primes_factors.hs
+++ solved/problem047_distinct_primes_factors.hs
@@ -18,16 +18,21 @@
 --     Find the first four consecutive integers to have four distinct
 -- 	prime factors. What is the first of these numbers?
 
+import Data.List
 import Primes
+
+primeGaps = filter (not . isPrime . head) $ groupBy (\a b -> isPrime a == isPrime b) [1..] 
 
 numDistinctPrimeFactors :: Integer -> Integer
 numDistinctPrimeFactors = fromIntegral . length . primeFactors
+
+dropLast n l = let k = length l - fromIntegral n in take k l
 
 main = do
     let k = 4 :: Integer
         factorsTest = (==k) . numDistinctPrimeFactors
         consecutiveTest n = all factorsTest [n..(n+k-1)]
-        first = head $ filter consecutiveTest [1..]
+        longGaps = filter (not . null) $ map (dropLast (k - 1)) primeGaps
+        first = head $ head $ filter (not . null) $ map (filter consecutiveTest) longGaps
         nums = [first..(first+k-1)]
     print $ zip nums (map primeFactors nums)
-    print first
