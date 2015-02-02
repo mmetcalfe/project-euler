597069cac180b547930975e6d5ece052cd11731f
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-02-02 20:29:57 +1100

    PE-050: Solve.

--- solved/problem050_consecutive_prime_sum.hs
+++ solved/problem050_consecutive_prime_sum.hs
@@ -17,26 +17,21 @@
 import Data.List
 import Primes
 
+consecutiveSums :: Integer -> [Integer] -> [Integer]
 -- consecutiveSums k l = foldr (zipWith (+)) l (take k $ tail $ tails l)
 consecutiveSums k l =
     let cumul = scanl (+) 0 l
-    in zipWith (-) (drop k cumul) cumul
+    in zipWith (-) (genericDrop k cumul) cumul
 
 interleavelists :: [[a]] -> [a]
 interleavelists = concat . transpose
 
 main = do
-    let n = 100 -- 10^6
+    let n = 10^6 -- 10^6
         primeRange = takeWhile (<n) primes
-        greatestPrime = last $ primeRange -- 999983
+        greatestPrime = last primeRange -- 999983
         longestSeq = fst $ last $  takeWhile ((<n) . snd) $ map (\x -> (x, (sum . flip take primes) x)) [1..]
-    -- print $ length primeRange
-    -- print greatestPrime
-    -- print longestSeq
-    -- let sums = map (flip consecutiveSums (filter (<= greatestPrime) primes)) [21, 23..longestSeq]
-    -- print $ head $ filter (not . null) $ map (filter isPrime) sums
         evenLengthSums = filter (even . snd) $ zip (map (:[]) (scanl1 (+) primeRange)) [1..] -- even length sums must start with 2
         oddLengthSums = map (\k -> (consecutiveSums k (tail primeRange), k)) [3, 5..]
-        -- sums = interleavelists [evenLengthSums, take longestSeq oddLengthSums]
-    -- print $ map head $ filter (not . null) $ map (filter isPrime) sums
-    print $ oddLengthSums
+        sums = interleavelists [evenLengthSums, take longestSeq oddLengthSums] :: [([Integer], Integer)]
+    print $ last $ filter (not . null . fst) $ map (\(a, b) -> (take 1  $ filter isPrime $ filter (<n) a, b)) sums
