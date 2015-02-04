fcb2dab496b0bef2ae27dae2586160a5d7cfbcc2
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-02-04 12:59:19 +1100

    PE-054: Add p054_poker.txt and begin implementing a Card data type.

--- unsolved/problem054_poker_hands.hs
+++ unsolved/problem054_poker_hands.hs
@@ -3,9 +3,9 @@
 -- (Published on Friday, 10th October 2003, 06:00 pm; Solved by 18901)
 -- 
 --     In the card game poker, a hand consists of five cards and are
--- 	ranked, from lowest to highest, in the following way:
+--  ranked, from lowest to highest, in the following way:
 -- 
---         - *High Card*: Highest value card.
+--     - *High Card*: Highest value card.
 --     - *One Pair*: Two cards of the same value.
 --     - *Two Pairs*: Two different pairs.
 --     - *Three of a Kind*: Three cards of the same value.
@@ -14,51 +14,61 @@
 --     - *Full House*: Three of a kind and a pair.
 --     - *Four of a Kind*: Four cards of the same value.
 --     - *Straight Flush*: All cards are consecutive values of same
--- 	suit.
+--        suit.
 --     - *Royal Flush*: Ten, Jack, Queen, King, Ace, in same suit.
 -- 
 -- 
 --     The cards are valued in the order:
--- 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace.
+--  2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace.
 -- 
 --     If two players have the same ranked hands then the rank made up
--- 	of the highest value wins; for example, a pair of eights beats a
--- 	pair of fives (see example 1 below). But if two ranks tie, for
--- 	example, both players have a pair of queens, then highest cards in
--- 	each hand are compared (see example 4 below); if the highest cards
--- 	tie then the next highest cards are compared, and so on.
+--  of the highest value wins; for example, a pair of eights beats a
+--  pair of fives (see example 1 below). But if two ranks tie, for
+--  example, both players have a pair of queens, then highest cards in
+--  each hand are compared (see example 4 below); if the highest cards
+--  tie then the next highest cards are compared, and so on.
 -- 
 --     Consider the following five hands dealt to two players:
 -- 
---          *Hand*         *Player 1*          *Player 2*
--- 	*Winner*
---         *1*         5H 5C 6S 7S KD
--- Pair of Fives           2C 3S 8S 8D TD
--- Pair of Eights          Player 2
---         *2*         5D 8C 9S JS AC
--- Highest card Ace            2C 5C 7D 8S QH
--- Highest card Queen          Player 1
---         *3*         2D 9C AS AH AC
--- Three Aces          3D 6D 7D TD QD
--- Flush  with Diamonds            Player 2
---         *4*         4D 6S 9H QH QC
--- Pair of Queens
--- Highest card Nine           3D 6D 7H QD QS
--- Pair of Queens
--- Highest card Seven          Player 1
---         *5*         2H 2D 4C 4D 4S
--- Full House
--- With Three Fours            3C 3D 3S 9S 9D
--- Full House
--- with Three Threes           Player 1
--- 
--- 
+--          *Hand*      *Player 1*          *Player 2*            *Winner*
+--           *1*      5H 5C 6S 7S KD       2C 3S 8S 8D TD         Player 2
+--                    Pair of Fives        Pair of Eights
+--           
+--           *2*      5D 8C 9S JS AC       2C 5C 7D 8S QH         Player 1
+--                    Highest card Ac      Highest card Queen  
+--           
+--           *3*      2D 9C AS AH AC       3D 6D 7D TD QD         Player 2
+--                    Three Aces           Flush  with Diamonds 
+--            
+--           *4*      4D 6S 9H QH QC       3D 6D 7H QD QS         Player 1
+--                    Pair of Queens       Pair of Queens
+--                    Highest card Nine    Highest card Seven
+--
+--           *5*      2H 2D 4C 4D 4S       3C 3D 3S 9S 9D         Player 1
+--                    Full House           Full House
+--                    With Three Fours     With Three Threes
+--
 --     The file, [poker.txt](project/resources/p054_poker.txt),
--- 	contains one-thousand random hands dealt to two players. Each line
--- 	of the file contains ten cards (separated by a single space): the
--- 	first five are Player 1's cards and the last five are Player 2's
--- 	cards. You can assume that all hands are valid (no invalid
--- 	characters or repeated cards), each player's hand is in no specific
--- 	order, and in each hand there is a clear winner.
+--  contains one-thousand random hands dealt to two players. Each line
+--  of the file contains ten cards (separated by a single space): the
+--  first five are Player 1's cards and the last five are Player 2's
+--  cards. You can assume that all hands are valid (no invalid
+--  characters or repeated cards), each player's hand is in no specific
+--  order, and in each hand there is a clear winner.
 -- 
 --     How many hands does Player 1 win?
+
+data Suit = H | D | C | S
+            deriving (Eq, Ord, Show, Read)
+
+data Rank = A | K | Q | J | Integer
+            deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+data Card = Card Rank Suit
+
+handsStr = "8C TS KC 9H 4S 7D 2S 5D 3S AC"
+
+hands = (read . words) handsStr :: [Card]
+
+main = do
+    print $ hands
